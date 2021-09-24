c1baf59e2a1465be05c9adfdfb16d50eeef3b85d
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-24 09:26:34 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the Function topFiveMovies
+def topFiveMovies(a, b, c, d, e):
+    
+    #return five different movies with a 
+    #space between each one
+    return a + " " + b + " " + c + " " + d + " " + e
+
+#print the funtion with the variables as strings
+print (topFiveMovies("a", "b", "c", "d", "e"))
+
+#print the function with five movies
+print (topFiveMovies ("Home Alone 2", 
+"Spider-Man: Into the Spider-Verse", 
+"Dragonball Super: Broly", "Dragonball Z: Fusion Reborn", 
+"The Amazing Spider-Man 2"))
+
